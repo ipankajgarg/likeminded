cd6dd26efae6f6aee268efb666b7d74ea121c6fa
Pankaj garg <gargpankaj901@gmail.com>
2020-02-12 12:47:55 +0530

    tab view using one app container

--- likeminded/src/editProfile/components/TabView.js
+++ likeminded/src/editProfile/components/TabView.js
@@ -1,63 +1,51 @@
 import React, {Component} from 'react';
 import {View, Text, Dimensions, StyleSheet} from 'react-native';
-import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
-import {createAppContainer} from 'react-navigation';
+// import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
+// import {createAppContainer} from 'react-navigation';
 import {NavigationContainer} from '@react-navigation/native';
-// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
-const FirstRoute = () => (
-  <View style={[styles.scene]}>
-    <Text>hello world</Text>
-  </View>
-);
-
-const SecondRoute = () => (
-  <View style={[styles.scene]}>
-    <Text>new world</Text>
-  </View>
-);
-
-const TabNavigator = createMaterialTopTabNavigator(
-  {
-    first: FirstRoute,
-    second: SecondRoute,
-  },
-  {
-    tabBarOptions: {
-      style: {
-        backgroundColor: 'white',
-      },
-      labelStyle: {
-        //fontWeight: 500,
-        fontSize: 15,
-      },
-      activeTintColor: 'black',
-      inactiveTintColor: 'grey',
-      indicatorStyle: {color: 'black', backgroundColor: 'black'},
-      pressOpacity: 0.2,
-    },
-    // swipeEnabled: true,
-  },
-);
-
-Navigator = createAppContainer(TabNavigator);
-
-class TabView extends Component {
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <Navigator />
-      </View>
-    );
-  }
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  scene: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
 
-export default TabView;
+const Tab = createMaterialTopTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        tabBarOptions={{
+          style: {
+            backgroundColor: 'white',
+          },
+          labelStyle: {
+            //fontWeight: 500,
+            fontSize: 15,
+            textTransform: 'capitalize',
+          },
+          activeTintColor: 'black',
+          inactiveTintColor: 'grey',
+          indicatorStyle: {color: 'black', backgroundColor: 'black'},
+          pressOpacity: 0.2,
+        }}>
+        <Tab.Screen name="Who like you" component={HomeScreen} />
+        <Tab.Screen name="Who you like" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// export default App;
