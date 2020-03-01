6915ff6c5b10a3696e504f9ab2b0edc0530c7e65
Pankaj garg <gargpankaj901@gmail.com>
2020-03-01 16:31:30 +0530

    edit about section

--- likeminded/src/screens/EditAbout.js
+++ likeminded/src/screens/EditAbout.js
@@ -1,18 +1,43 @@
 import React, {Component} from 'react';
 import {Text, View, TextInput, StyleSheet, Platform} from 'react-native';
+import {updateAboutMe} from '../mutations/editAboutMutation';
+import {graphql} from 'react-apollo';
 
 class EditAbout extends Component {
+  state = {about: ''};
+
+  onChange = about => {
+    this.setState({about});
+  };
+
+  onSubmit = () => {
+    console.log('in function');
+    const {mutate, navigation} = this.props;
+    const {about} = this.state;
+
+    mutate({variables: {id: '5d6678e761a5793aacb42c0c', about}})
+      .then(data => {
+        navigation.navigate('EditProfile');
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
     const {inputStyle} = styles;
+    const {about} = this.state;
 
     return (
       <View style={inputStyle}>
         <TextInput
+          value={about}
+          onChangeText={text => this.onChange(text)}
           placeholder="About yourself"
           style={{paddingHorizontal: 10}}
           autoFocus={true}
           multiline={true}
         />
+
+        <Text onPress={this.onSubmit}>click me</Text>
       </View>
     );
   }
@@ -29,4 +54,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default EditAbout;
+export default graphql(updateAboutMe)(EditAbout);
