0d8432652a453ed8c378459df31b896f3b76e799
Pankaj garg <gargpankaj901@gmail.com>
2020-05-10 20:35:11 +0530

    search in app

---
+++ likeminded/src/screens/Search.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import {View, Text, TextInput} from 'react-native';
+import {gql} from 'apollo-boost';
+import {withApollo, graphql} from 'react-apollo';
+
+class Search extends Component {
+  state = {isLoading: false, data: []};
+
+  onChangeText = name => {
+    const {client} = this.props;
+    this.setState({isLoading: true});
+    name = name.toLowerCase();
+    client
+      .query({query, variables: {name}})
+      .then(({data: {searchedProfiles}}) => {
+        console.log(searchedProfiles);
+        this.setState({data: searchedProfiles, isLoading: false});
+      })
+      .catch(err => console.log(err));
+  };
+
+  debounce = () => {
+    let timeout;
+
+    return name => {
+      clearTimeout(timeout);
+
+      timeout = setTimeout(() => this.onChangeText(name), 500);
+    };
+  };
+
+  render() {
+    console.log('props', this.props);
+    const {isLoading, data} = this.state;
+    const debounce = this.debounce();
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{marginTop: 40, marginHorizontal: 'auto'}}>
+          <View
+            style={{
+              borderRadius: 20,
+              backgroundColor: '#F9F9F9',
+              padding: 10,
+              minWidth: '75%',
+            }}>
+            <TextInput
+              onChangeText={text => debounce(text)}
+              autoFocus
+              placeholder="Search"
+            />
+          </View>
+
+          {data.map(profile => (
+            <View>
+              <Text>{profile.name}</Text>
+            </View>
+          ))}
+          {isLoading && <Text>Loading...</Text>}
+        </View>
+      </View>
+    );
+  }
+}
+
+const query = gql`
+  query SearchedProfiles($name: String!) {
+    searchedProfiles(name: $name) {
+      name
+      about
+      profileImage
+    }
+  }
+`;
+
+export default withApollo(Search);
