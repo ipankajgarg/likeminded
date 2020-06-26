7e55671b41fe84ae61b595da1566b6c6cfe5df9e
Pankaj garg <gargpankaj901@gmail.com>
2020-06-26 23:31:05 +0530

    reusable animated Component

--- likeminded/src/common/components/Animation.js
+++ likeminded/src/common/components/Animation.js
@@ -15,34 +15,7 @@ const deviceWidth = Dimensions.get('window').width;
 const deviceHeight = Dimensions.get('window').height;
 const statusBarHeight = StatusBar.currentHeight;
 
-const dummy = [
-  {
-    text: 'Click me 1',
-    callBack() {
-      console.log('click me1');
-    },
-  },
-  {
-    text: 'Click me 2',
-    callBack() {
-      console.log('click me2');
-    },
-  },
-  {
-    text: 'Click me 3',
-    callBack() {
-      console.log('click me3');
-    },
-  },
-  {
-    text: 'Click me 4',
-    callBack() {
-      console.log('click me4');
-    },
-  },
-];
-
-class Ball extends Component {
+class Animation extends Component {
   position = new Animated.ValueXY({
     x: 0,
     y: deviceHeight,
@@ -53,7 +26,6 @@ class Ball extends Component {
 
     this.state = {
       dimensions: undefined,
-
       position: this.position,
     };
   }
@@ -62,6 +34,22 @@ class Ball extends Component {
     if (this.props.visible && this.state.dimensions) {
       this.startAnimate();
     }
+  }
+
+  // componentWillUpdate(nextProps) {
+  //   if (!nextProps.visible && this.props.visible) {
+  //     console.log('closing');
+  //     this.onClose();
+  //   }
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (!nextProps.visible && this.props.visible) {
+      console.log('closing');
+      this.onClose();
+      return false;
+    }
+    return true;
   }
 
   startAnimate = () => {
@@ -76,16 +64,17 @@ class Ball extends Component {
             ? deviceHeight - height
             : deviceHeight - height - statusBarHeight,
       },
-      // easing: Easing.back(),
-      // delay: 5000,
-      //   overshootClamping: true,
+      //duration: 5000,
+      //easing: Easing.back(),
+      // delay: 2000,
+      //overshootClamping: true,
     }).start();
     // this.setState({showLayer: true});
   };
   onClose = () => {
     Animated.timing(this.state.position, {
       toValue: {x: 0, y: deviceHeight},
-      duration: 150,
+      duration: 200,
       //easing: Easing.back(),
     }).start(({finished}) => {
       if (finished) {
@@ -103,24 +92,15 @@ class Ball extends Component {
   };
 
   render() {
-    const {menu, textColor, backgroundColor, visible} = this.props;
+    const {visible, backgroundLayerColor} = this.props;
     console.log('position layout', this.state.position);
     if (visible)
       return (
         <View
           style={{
             position: 'absolute',
-            zIndex: 1,
+            // zIndex: 1,
           }}>
-          {/* <View
-          style={{
-            // position: 'absolute',
-            // top: 0,
-            height: 100,
-            width: deviceWidth,
-            backgroundColor: 'black',
-          }}></View> */}
-
           <Animated.View
             style={[
               this.state.position.getLayout(),
@@ -130,62 +110,24 @@ class Ball extends Component {
                 zIndex: 100,
               },
             ]}>
-            <View
-              onLayout={this.onLayout}
-              style={{
-                backgroundColor: backgroundColor || 'white',
-                borderTopLeftRadius: 30,
-                borderTopRightRadius: 30,
-              }}>
-              {menu.map(({text, callback}, index) => (
-                <TouchableOpacity
-                  key={index}
-                  onPress={() => {
-                    callback();
-                    this.onClose();
-                  }}
-                  style={{borderBottomWidth: 0.3, borderColor: 'lightgrey'}}>
-                  <Text
-                    style={{
-                      marginLeft: 20,
-                      // marginBottom: 5,
-                      paddingVertical: 20,
-                      paddingHorizontal: 10,
-                      color: textColor || 'black',
-                    }}>
-                    {text}
-                  </Text>
-                </TouchableOpacity>
-              ))}
-            </View>
+            <View onLayout={this.onLayout}>{this.props.children}</View>
           </Animated.View>
           <TouchableWithoutFeedback onPress={this.onClose}>
             <View
               style={{
                 position: 'absolute',
                 top: 0,
-                // left: 50,
-                zIndex: 50,
-
                 width: deviceWidth,
                 height: deviceHeight,
-                backgroundColor: 'rgba(211,211,211,0.15)',
+                backgroundColor:
+                  backgroundLayerColor || 'rgba(211,211,211,0.15)',
               }}></View>
           </TouchableWithoutFeedback>
         </View>
       );
+
     return null;
   }
 }
 
-const styles = {
-  ball: {
-    height: 60,
-    width: 60,
-    borderRadius: 30,
-    borderWidth: 30,
-    borderColor: 'black',
-  },
-};
-
-export default Ball;
+export default Animation;
